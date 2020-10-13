52cf2cc704a8e5c24d65e4943e229f27e3d09177
Phillip <degenaarp@gmail.com>
2020-10-13 14:56:02 +0200

    added port config

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("This is the first page of the server");
+    res.send("This is the first page of the server blab blab");
 });
 
-app.listen(8080, () => {
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
     console.log("Server runnig again.....");
 });
 
